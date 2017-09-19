6cfefe84a77df8dd7304a78332283bc255e04b58
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-19 09:16:40 -0600

    add iteration 1:inventory

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,5 +1,14 @@
 class Inventory
 
-  
+  attr_reader :date, :items
+
+  def initialize(date)
+    @date = date
+    @items = {}
+  end
+
+  def record_item(item)
+    @items.merge!(item)
+  end
 
 end
--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -1,9 +1,32 @@
 require "minitest/autorun"
 require "minitest/pride"
+require "date"
 require './lib/inventory'
 
 class InventoryTest < Minitest::Test
 
+  attr_reader :inventory1
 
+  def setup
+    @inventory1 = Inventory.new(Date.new(2017, 9, 18))
+  end
+
+  def test_inventory_exists
+    assert_instance_of Inventory, inventory1
+  end
+
+  def test_inventory_has_a_date
+    assert_equal Date.new(2017, 9, 18), inventory1.date
+  end
+
+  def test_inventory_has_items
+    assert_instance_of Hash, inventory1.items
+  end
+
+  def test_inventory_can_record_new_items
+    inventory1.record_item({"shirt" => {"quantity" => 50, "cost" => 15}})
+
+    assert_equal ({"shirt" => {"quantity" => 50, "cost" => 15}}), inventory1.items
+  end
 
 end
