fbba150505bce5e6360248ef4cab1b2b971f42e1
BLaurenB <blbillington1@gmail.com>
2017-09-19 11:55:45 -0600

    small update

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -12,15 +12,7 @@ class Inventory
 
   #this method both adds new and updates existing item hashes
   def record_item(item_qty_cost)
-      # require "pry"; binding.pry
-  item = item_qty_cost.keys[0]  #good
-  qty_and_cost = item_qty_cost.values[0]  #good
-  @items = {item => qty_and_cost}
-  #good {"shirt" => {"quantity" => 50, "cost" => 15}}
-  end
 
-  def record_item(item_qty_cost)
-  # require "pry"; binding.pry
     item = item_qty_cost.keys[0]  #good
     qty_and_cost = item_qty_cost.values[0]  #good
 
@@ -30,6 +22,7 @@ class Inventory
         @items = {item => qty_and_cost}
       else items[name] == item_qty_cost.keys
       #   see if there is already an item of the same name and add to the values
+        require "pry"; binding.pry
         quantity = qty_and_cost.keys[0]  #
         amount = qty_and_cost.values[0] #
         quantities = {quantity => amount} #
