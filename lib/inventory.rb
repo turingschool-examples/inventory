acdecc1e0bea8cc256ecf42309fb5be751b4430f
BLaurenB <blbillington1@gmail.com>
2017-09-19 11:59:17 -0600

    updating while I still can

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -12,21 +12,21 @@ class Inventory
 
   #this method both adds new and updates existing item hashes
   def record_item(item_qty_cost)
-
     item = item_qty_cost.keys[0]  #good
     qty_and_cost = item_qty_cost.values[0]  #good
 
-    # if there's nothing in the @items hash,
-    #   just add the item.
-      if items.empty?  ||  items[item] != item_qty_cost.keys
+    # if there's nothing in the @items hash, just add the item.
+      if items.empty?
         @items = {item => qty_and_cost}
-      else items[name] == item_qty_cost.keys
+      elsif items[item] == item_qty_cost.keys
       #   see if there is already an item of the same name and add to the values
-        require "pry"; binding.pry
+        # require "pry"; binding.pry
         quantity = qty_and_cost.keys[0]  #
         amount = qty_and_cost.values[0] #
         quantities = {quantity => amount} #
         @items = items[item][quantity]  + items[item][quantity]  #
+      else
+        @items = {item => qty_and_cost}
       end
     end
 
