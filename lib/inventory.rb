0b956f685ee5908b50357b1ddba9ef8d934c1623
Hugh Morrison <hmorri32@gmail.com>
2017-09-19 09:16:05 -0600

    hardcoded iteration 1. fix later.

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,5 +1,14 @@
+require 'date'
 class Inventory
+  attr_reader :date, :items
 
-  
+  def initialize(date)
+    @date = date
+    @items = Hash.new
+  end
+
+  def record_item(hash)
+    @items = hash
+  end
 
 end
--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -1,9 +1,33 @@
 require "minitest/autorun"
 require "minitest/pride"
 require './lib/inventory'
+require 'date'
 
 class InventoryTest < Minitest::Test
+  attr_reader :inventory1
+  def setup
+    @inventory1 = Inventory.new(Date.new(2017, 9, 18))
+  end
 
+  def test_exists
+    assert_instance_of Inventory, inventory1
+  end
 
+  def test_inventory_records_date
+    assert_instance_of Date, inventory1.date
+  end
+
+  def test_inventory_starts_with_no_items 
+    assert_equal ({}), inventory1.items    
+  end
+
+  def test_inventory_can_record_item
+    assert_equal ({}), inventory1.items   
+
+    arg = {"shirt" => {"quantity" => 50, "cost" => 15}} 
+    inventory1.record_item(arg)   
+     
+    assert_equal arg, inventory1.items
+  end
 
 end
