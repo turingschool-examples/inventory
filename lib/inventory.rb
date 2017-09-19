0d63c1100ca97820a06249e3a32cea460596a9e3
Emily Wise <wise.m.emily@gmail.com>
2017-09-19 09:27:29 -0600

    Add Items and Date to Inv

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,8 +1,11 @@
 require 'date'
 class Inventory
 
+  attr_reader :date, :items
+
   def initialize(date)
     @date = date
+    @items = {}
   end
 
 
--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -15,6 +15,18 @@ class InventoryTest < Minitest::Test
     assert_instance_of Inventory, @inventory_1
   end
 
+  def test_inventory_has_a_date
+    expected = Date.new(2017, 9, 18)
+
+    assert_instance_of Date, @inventory_1.date
+    assert_equal expected, @inventory_1.date
+  end
+
+  def test_inventory_has_items_and_is_empty_on_initialization
+    assert_instance_of Hash, @inventory_1.items
+    assert @inventory_1.items.empty?
+  end
+
 
 
 end
