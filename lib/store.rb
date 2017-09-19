210270663e05163742e742ba7f67268a6b75bf69
Hugh Morrison <hmorri32@gmail.com>
2017-09-19 11:30:18 -0600

    iteration 4, refactor

--- lib/store.rb
+++ lib/store.rb
@@ -17,8 +17,27 @@ class Store
   end
 
   def stock_check(item)
-    inventory_record.map do |inventory|
-      inventory.check_inventory(item)
-    end.compact.first
+    inventory_record.map {|inv| inv.check_inventory(item)}
+  end
+
+  def amount_sold(item)
+    item_count = stock_check(item).map {|list| list['quantity']}
+    item_count.inject {|total, quantity| total -= quantity}
+  end
+
+  def us_order(item)
+    item.map {|item, value| cost(item) * value}.sum
+  end
+
+  def print_us_order(item)
+    return "$#{us_order(item)}"    
+  end
+
+  def cost(item)
+    inventory_record.flat_map {|inv| inv.item_cost(item)}[0]
+  end
+
+  def brazilian_order(item)
+    return "R$#{'%.2f' % (us_order(item).to_f * 3.08).floor(2)}"
   end
 end
