3d3b11a87e40c5f7d3a4a13a7a9e15f24ee0b4f5
Marshall Houston <marshall.houston@gmail.com>
2017-09-19 11:23:46 -0600

    add new test for multiple inventories

--- test/store_test.rb
+++ test/store_test.rb
@@ -42,15 +42,30 @@ class StoreTest < Minitest::Test
     assert_instance_of Inventory, store.inventory_record[0]
   end
 
-  def test_it_can_check_stock_of_inventory
+  def test_it_can_add_multiple_inventories
     store = Store.new("Ace", "834 2nd St", "Hardware")
-    inventory = Inventory.new(Date.new(2017, 9, 18))
-    inventory.record_item({"shirt" => {"quantity" => 50, "cost" => 15}})
-    store.add_inventory(inventory)
+    inventory1 = Inventory.new(Date.new(2017, 9, 18))
+    inventory1.record_item({"shirt" => {"quantity" => 50, "cost" => 15}})
+    inventory2 = Inventory.new(Date.new(2017, 9, 18))
+    inventory2.record_item({"flies" => {"quantity" => 20, "cost" => 15}})
 
-    assert_equal 50, store.stock_check('shirt')
+    store.add_inventory(inventory1)
+    store.add_inventory(inventory2)
+
+    assert_equal 2, store.inventory_record.count
+    assert_equal 'flies', store.inventory_record.last.item_name
   end
 
-  
+  def test_it_can_check_stock_of_inventory
+    store = Store.new("Ace", "834 2nd St", "Hardware")
+    inventory1 = Inventory.new(Date.new(2017, 9, 18))
+    inventory1.record_item({"shirt" => {"quantity" => 50, "cost" => 15}})
+    inventory1.record_item({"shirt" => {"quantity" => 10, "cost" => 15}})
+    store.add_inventory(inventory1)
+
+    assert_equal 60, store.stock_check('shirt')
+  end
+
+
 
 end
