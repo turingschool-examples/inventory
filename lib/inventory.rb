56f58bc20b4f845f8b332ad0e4c5cd76ec5701dd
Aurora Ziobrowski <aziobrow@gmail.com>
2017-09-19 10:17:13 -0600

    finally, finally got record_item to pass

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -9,19 +9,24 @@ class Inventory
     @items = {}
   end
 
-  def merge_item_quantities(old_quantity, new_quantity)
-    old_quantity.merge(new_quantity) do |key, first_amount, second_amount|
-      first_amount + second_amount
+  def merge_item_quantities(old_values, new_values)
+    old_quantity = old_values.select {|key,value| key == 'quantity'}
+    new_quantity = new_values.select {|key,value| key == 'quantity'}
+    old_quantity.merge(new_quantity) do |key, old_quantity, new_quantity|
+      old_quantity + new_quantity
     end
   end
 
-  def record_item(item_info)
-    @items = @items.merge(item_info) do |item, old_value, new_value|
-      old_quantity = old_value.select {|key,value| key == 'quantity'}
-      new_quantity = new_value.select {|key,value| key == 'quantity'}
-      require "pry"; binding.pry
-    end
+  def item_cost(new_values)
+    new_values.select {|key,value| key == 'cost'}
+  end
 
+  def record_item(item_info)
+    @items = @items.merge(item_info) do |item, old_values, new_values|
+      updated_quantity = merge_item_quantities(old_values, new_values)
+      cost = item_cost(new_values)
+      item_info[item] = updated_quantity.merge(cost)
+    end
   end
 
 
