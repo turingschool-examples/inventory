5ffd35d5e4c03c9e21927bc1c221a7fcbabf4634
Aurora Ziobrowski <aziobrow@gmail.com>
2017-09-19 09:46:18 -0600

    still struggling along with record item summing

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -10,9 +10,17 @@ class Inventory
   end
 
   def record_item(item_info)
-    @items.reduce({}) do |sum, item_info|
-
-    @items = @items.merge(item_info) {|key, old_value, new_value| old_value + new_value}
+    # @items.reduce({}) do |@item, item_info|
+    #   @items.merge(item_info) do |quantity, cost|
+    @items = @items.merge(item_info) do |item, old_value, new_value|
+      old_value.merge(new_value) do |key, quantity, cost|
+        require "pry"; binding.pry
+        old_quantity + new_quantity
+      end
+      old_value.merge(new_value) do |cost, old_cost, new_cost|
+        old_cost + new_cost
+      end
+    end
   end
 
 
