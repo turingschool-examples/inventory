5bab1e2a29dcdbd84bca2aaca012408ad0bdf051
Miguel Silva <msilva0897@gmail.com>
2017-09-19 09:09:33 -0600

    test for inventory

--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -3,7 +3,27 @@ require "minitest/pride"
 require './lib/inventory'
 
 class InventoryTest < Minitest::Test
+  attr_reader :inventory
 
+  def setup
+    @inventory = Inventory.new(Date.new(2017, 9, 18))
+  end
+
+  def test_if_instance_of_a_class_exist
+    assert_instance_of Inventory, inventory
+  end
+
+  def test_if_date_records_date
+    assert_equal Date, inventory.date
+  end
+
+  def test_if_items_is_a_hash
+    assert_equal {}, repository.items
+  end
+
+  def test_if_items_records_the_item
+
+  end
 
 
 end
