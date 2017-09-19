6763b13f33de2400aae2fa908223b678aa16f6dd
Miguel Silva <msilva0897@gmail.com>
2017-09-19 11:30:40 -0600

    new test

--- test/store_test.rb
+++ test/store_test.rb
@@ -54,6 +54,18 @@ class StoreTest < Minitest::Test
     assert_equal ({"quantity" => 60, "cost" => 15}), acme.stock_check("shirt")
   end
 
+  def test_if_stock_returns_inventory_array
+    acme = Store.new("Acme", "324 Main St", "Grocery")
+    inventory1 = Inventory.new(Date.new(2017, 9, 18))
+    inventory2 = Inventory.new(Date.new(2017, 9, 18))
+    inventory1.record_item({"shirt" => {"quantity" => 60, "cost" => 15}})
+    inventory2.record_item({"shoes" => {"quantity" => 40, "cost" => 30}})
+    acme.add_inventory(inventory1)
+    acme.add_inventory(inventory2)
+
+    assert_equal Inventory, acme.stock("shirt").class
+  end
+
   def test_if_amount_sold_returns_integer
     inventory3 = Inventory.new(Date.new(2017, 9, 16))
     inventory4 = Inventory.new(Date.new(2017, 9, 18))
