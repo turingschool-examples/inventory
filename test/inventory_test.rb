677df7ee6921690cea849d2372ed08810e3250cf
Dione Wilson <dionew1@gmail.com>
2017-09-19 09:20:26 -0600

    Inventory exists with date

--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -1,9 +1,19 @@
 require "minitest/autorun"
 require "minitest/pride"
 require './lib/inventory'
+require 'date'
 
 class InventoryTest < Minitest::Test
 
+  def test_inventory_exists
+    inventory = Inventory.new(Date.new(2017, 9, 18))
 
+    assert_instance_of Inventory, inventory
+  end
 
+  def test_inventory_has_date
+    inventory = Inventory.new(Date.new(2017, 9, 18))
+
+    assert_instance_of Date, inventory.date
+  end
 end
