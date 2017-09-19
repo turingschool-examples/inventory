d27464df391fdb6ab9da1a31bec3c2d8409beb92
Erika Smith <erikaannesmith@gmail.com>
2017-09-19 10:58:58 -0600

    Add ability to check amount of an item sold

--- lib/store.rb
+++ lib/store.rb
@@ -25,4 +25,18 @@ class Store
     end.items.values[0]
   end
 
+  def amount_sold(item)
+    q = determine_quantities(item)
+    (q[0]["quantity"]) - (q[1]["quantity"])
+  end
+
+  def determine_quantities(item)
+    invoices_with_item = @inventory_record.find_all do |inv|
+      inv.items.keys.include?(item)
+    end
+    invoices_with_item.map do |inv|
+      inv.items[item]
+    end
+  end
+
 end
--- test/store_test.rb
+++ test/store_test.rb
@@ -79,4 +79,16 @@ class StoreTest < Minitest::Test
     assert_equal 30, acme.stock_check("shirt")["cost"]
   end
 
+  def test_store_can_check_amount_sold
+    ace = Store.new("Ace", "834 2nd St", "Hardware")
+    inventory3 = Inventory.new(Date.new(2017, 9, 16))
+    inventory3.record_item({"hammer" => {"quantity" => 20, "cost" => 20}})
+    inventory4 = Inventory.new(Date.new(2017, 9, 18))
+    inventory4.record_item({"mitre saw" => {"quantity" => 10, "cost" => 409}})
+    inventory4.record_item({"hammer" => {"quantity" => 15, "cost" => 20}})
+    ace.add_inventory(inventory3)
+    ace.add_inventory(inventory4)
+
+    assert_equal 5, ace.amount_sold("hammer") 
+  end
 end
