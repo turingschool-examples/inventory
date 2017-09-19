9783f5a1ae829e9fcf6304a86c585d0312544f60
Adrian Lara <adrianblara@gmail.com>
2017-09-19 11:11:01 -0600

    amount sold written

--- lib/store.rb
+++ lib/store.rb
@@ -30,11 +30,18 @@ class Store
 
   def sort_by_most_recent(inventories)
     descend = -1
-    inventories.sort_by { |inventory| descend * inventory.date.to_time.to_i }
+    inventories.sort_by do |inventory|
+      descend * inventory.date.to_time.to_i
+    end
   end
 
   def amount_sold(item)
+    unsorted_inventories = find_inventories_with_item(item)
+    inventories = sort_by_most_recent(unsorted_inventories)
+    latest_data = inventories[0].items[item]
+    previous_data = inventories[1].items[item]
 
+    previous_data["quantity"] - latest_data["quantity"]
   end
 
 end
