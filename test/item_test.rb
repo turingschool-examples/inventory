b31b44821c14424adcb9bae3075e1521c4ef60e8
Marshall Houston <marshall.houston@gmail.com>
2017-09-19 11:53:42 -0600

    add existence test to item

--- test/item_test.rb
+++ test/item_test.rb
@@ -3,12 +3,16 @@ require "minitest/pride"
 require './lib/item'
 
 class ItemTest < Minitest::Test
+
   def test_it_exists
     item = Item.new({"shirt" => {"quantity" => 50, "cost" => 15}})
 
+    assert_instance_of Item, item
     assert_equal 'shirt', item.name
     assert_equal 50, item.quantity
     assert_equal 15, item.cost
   end
+
+
 
 end
