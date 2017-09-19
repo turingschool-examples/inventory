28777b04f7c148d996f99be45470c1fa253d320e
Tyjoo26 <Tyjoo21@gmail.com>
2017-09-19 09:09:02 -0600

    Require  Date class and add  test it exists

--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -1,9 +1,15 @@
 require "minitest/autorun"
 require "minitest/pride"
 require './lib/inventory'
+require 'date'
+
 
 class InventoryTest < Minitest::Test
+  def test_it_exists
+    inventory = Inventory.new(Date.new(2017, 9, 18))
 
+    assert_instance_of Inventory, inventory
+  end
 
 
 end
