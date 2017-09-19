a0328a7948c104fcb93bfbe49db631b2bf1393f5
Marshall Houston <marshall.houston@gmail.com>
2017-09-19 09:14:29 -0600

    add passing test for inventory date

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,9 +1,10 @@
 require 'date'
 
 class Inventory
-
+  attr_reader :date
+  
   def initialize(date)
-
+    @date = date
   end
 
 end
--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -6,14 +6,15 @@ require 'date'
 class InventoryTest < Minitest::Test
 
   def setup
-     inventory = Inventory.new(Date.new(2017, 9, 18))
+     @inventory = Inventory.new(Date.new(2017, 9, 18))
   end
+
   def test_it_exists
-    assert_instance_of Inventory, Inventory.new(Date.new(2017, 9, 18))
+    assert_instance_of Inventory, @inventory
   end
 
-  def test_case_name
-
+  def test_it_has_a_date
+    assert_instance_of Date, @inventory.date
   end
 
 end
