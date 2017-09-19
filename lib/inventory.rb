379891441a02ebf403f64c468441abf31df331d3
Erika Smith <erikaannesmith@gmail.com>
2017-09-19 09:33:00 -0600

    Create inventory class and begin to build out functionality. Minitests are passing.

--- lib/inventory.rb
+++ lib/inventory.rb
@@ -1,5 +1,16 @@
-class Inventory
+require 'date'
 
-  
+class Inventory
+  attr_reader :date, :items
+
+  def initialize(date)
+    @date = date
+    @items = {}
+  end
+
+  def record_item(item)
+    # Add the item to the inventory hash 
+    items.merge!(item) {|k,v1,v2| v1+v2}
+  end
 
 end
--- test/inventory_test.rb
+++ test/inventory_test.rb
@@ -3,7 +3,28 @@ require "minitest/pride"
 require './lib/inventory'
 
 class InventoryTest < Minitest::Test
+  attr_reader :inv
 
+  def setup
+    @inv = Inventory.new(Date.new(2017, 9, 18))
+  end
 
+  def test_it_exists
+    assert_instance_of Inventory, inv
+  end
+
+  def test_it_holds_a_date
+    assert_instance_of Date, inv.date
+  end
+
+  def test_items_initializes_as_an_empty_hash
+    assert inv.items.empty?
+  end
+
+  def test_it_can_record_an_item_and_it_will_appear_in_items
+    inv.record_item({"shirt" => {"quantity" => 50, "cost" => 15}})
+
+    assert_equal 1, inv.items.count
+  end
 
 end
